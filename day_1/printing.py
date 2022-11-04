f5f4bee245bba2fa383a09c07138fdcd2226f186
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-11-04 23:51:39 +0430

    practing input method

--- day_1/printing.py
+++ day_1/printing.py
@@ -1,2 +1,15 @@
 # Writng your code below this line
 print("Hello Python")
+
+# String manipulation
+# \n new line
+print("Hello Python\nHello Python\nHello Python")
+# combining string
+print ("Hello" + " Sediq")
+
+# input method how to input text
+
+print (input("Enter your name: "))
+name = input("What is your name?")
+
+print ("Your Name is ", name)
