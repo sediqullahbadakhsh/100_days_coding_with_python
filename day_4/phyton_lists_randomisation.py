3cdc334ec26978049c4d8627b4779afc442d7699
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-11-14 01:01:10 +0430

    solve heal tail challenge

---
+++ day_4/phyton_lists_randomisation.py
@@ -0,0 +1,18 @@
+import random
+
+random_integer = random.randint(1,10)
+print(random_integer)
+
+random_float = random.random()
+# 0.00000 - 0.999999
+print(random_float)
+
+## List
+
+states_of_america = ["Delaware", "Pennsylvania"]
+print(states_of_america[0])
+
+states_of_america.append("New Jersey")
+states_of_america.extend("New York")
+print (states_of_america)
+
--- phyton_lists_randomisation.py
+++
@@ -1,8 +0,0 @@
-import random
-
-random_integer = random.randint(1,10)
-print(random_integer)
-
-random_float = random.random()
-# 0.00000 - 0.999999
-print(random_float)
